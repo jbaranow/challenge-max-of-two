80b7c105babd50ddc184c6c14af1c25dc0f92224
Julian Baranowski <julian.paul.baranowski@gmail.com>
2022-11-17 19:10:47 -0700

    First commit

---
+++ maxOfTwo.js
@@ -0,0 +1,11 @@
+let x = 1
+let y = 2
+
+// Write your code below this line.
+if (x > y) {
+    console.log('x');
+} else if(y > x) {
+    console.log('y');
+} else (x === y) {
+    console.log('The vales are the same.')
+}
